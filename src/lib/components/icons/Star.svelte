77c4a08a78cce913f48b89885db26009518c10fb
Sharon Seungyu Han <87476439+sharonsyh@users.noreply.github.com>
2024-05-28 21:49:37 +0900

    Add files via upload

---
+++ src/lib/components/icons/Star.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	export let className = 'w-4 h-4';
+	export let strokeWidth = '1.5';
+    export let filled = false;
+</script>
+{#if filled}
+<svg
+	xmlns="http://www.w3.org/2000/svg"
+    fill="currentColor"
+	viewBox="0 0 24 24"
+	stroke-width={strokeWidth}
+	stroke="currentColor"
+	class={className}
+>
+	<path stroke-linecap="round" stroke-linejoin="round" d="M7.5,0l-2,5h-5l4,3.5l-2,6l5-3.5&#xA;&#x9;l5,3.5l-2-6l4-3.5h-5L7.5,0z" />
+</svg>
+{:else}
+<svg
+	xmlns="http://www.w3.org/2000/svg"
+	fill="none"
+	viewBox="0 0 24 24"
+	stroke-width={strokeWidth}
+	stroke="currentColor"
+	class={className}
+>
+	<path stroke-linecap="round" stroke-linejoin="round" d="M7.5,0l-2,5h-5l4,3.5l-2,6l5-3.5&#xA;&#x9;l5,3.5l-2-6l4-3.5h-5L7.5,0z" />
+</svg>
+{/if}
